5b1e8138a54d9406c7c77ce82b005e9564f4e7ff
JL07 <34646435+jlapplications@users.noreply.github.com>
2018-06-06 22:02:36 +1000

    Update AutoMode.bat

--- Windows/AutoTSS/AutoMode.bat
+++ Windows/AutoTSS/AutoMode.bat
@@ -1,6 +1,7 @@
-@ECHO OFF
+@echo OFF
 TITLE "AutoTSS - AutoMode"
+set mypath=%cd%
 :AutoMode
-python "C:\Users\%USERNAME%\Documents\AutoTSS\autotss.py"
+python "%mypath%\autotss.py"
 timeout /t 30
 goto AutoMode
